5f9494d9dfcac5c63dd79dbe655f387dd5d0280b
fadliselaz <fadliselaz@gmail.com>
2020-06-16 20:51:45 +0700

    update pertama

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+
+const Card = (props)=>{
+
+    return(
+        <div className="card">
+            <h2 className="cardTitle">
+                {props.judul || 'title card' }
+            </h2>
+            <hr/>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum eveniet sint consequatur quam consequuntur ab quidem esse aut illum dolores obcaecati quo accusantium laborum, blanditiis voluptatibus aspernatur ad iusto!
+            </p>
+
+        </div>
+    )
+
+}
---
+++ src/index.jsx
@@ -0,0 +1,46 @@
+
+
+// MEMBUAT FUNC COMPONENT REACT
+const App = ()=>{
+
+    return (
+        <>
+        <div className="navbar" >
+            <h1 className="brand">
+                jvalley
+            </h1>
+
+            <div className="menu">
+                <a href="#">home</a>
+                <a href="#">about</a>
+                <a href="#">contact</a>
+                <a href="#">gallery</a>
+            </div>
+        </div>
+
+        <div className="container">
+
+            <Card judul='card pertama' />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+
+        </div>
+
+
+        <div className="footer" >
+            <small>@jvalley2020</small>
+        </div>
+
+        </>
+    )
+}
+
+// SELEKSI ELEMENT UNTUK PENEMPATAN RENDER
+const root = document.getElementById('root')
+
+//PROSES RENDERING
+ReactDOM.render( <App /> , root )
+
